4408a79cfd7f99efe63ab762346273cbab5c8126
Danijela Arsic <arsicdanijela8@gmail.com>
2024-03-02 15:11:53 +0100

    Postavljena Login kao pocetna strana

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,32 +4,32 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'log-in',
     pathMatch: 'full'
   },
   {
     path: 'add-new-book',
-    loadChildren: () => import('./add-new-book/add-new-book.module').then( m => m.AddNewBookPageModule)
+    loadChildren: () => import('./add-new-book/add-new-book.module').then(m => m.AddNewBookPageModule)
   },
   {
     path: 'edit-book',
-    loadChildren: () => import('./edit-book/edit-book.module').then( m => m.EditBookPageModule)
+    loadChildren: () => import('./edit-book/edit-book.module').then(m => m.EditBookPageModule)
   },
   {
     path: 'book-info',
-    loadChildren: () => import('./book-info/book-info.module').then( m => m.BookInfoPageModule)
+    loadChildren: () => import('./book-info/book-info.module').then(m => m.BookInfoPageModule)
   },
   {
     path: 'log-in',
-    loadChildren: () => import('./log-in/log-in.module').then( m => m.LogInPageModule)
+    loadChildren: () => import('./log-in/log-in.module').then(m => m.LogInPageModule)
   },
   {
     path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule)
   },
 ];
 
