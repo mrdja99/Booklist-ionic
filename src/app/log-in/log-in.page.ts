20a94d70c18ce2b5db86c3926248892a085b0388
Danijela Arsic <arsicdanijela8@gmail.com>
2024-03-02 15:21:23 +0100

    fix login page

--- src/app/log-in/log-in.page.ts
+++ src/app/log-in/log-in.page.ts
@@ -2,14 +2,14 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataService } from '../service/data.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.page.html',
   styleUrls: ['./log-in.page.scss'],
 })
-export class LogInPage implements OnInit, OnDestroy{
+export class LogInPage implements OnInit, OnDestroy {
 
   users: any;
 
@@ -18,7 +18,7 @@ export class LogInPage implements OnInit, OnDestroy{
   userName!: string;
   userSurname!: string;
 
-  sub:Subscription = new Subscription;
+  sub: Subscription = new Subscription;
 
   constructor(private dataService: DataService, private router: Router, private alertCtrl: AlertController) { }
 
@@ -31,38 +31,51 @@ export class LogInPage implements OnInit, OnDestroy{
   }
 
   getData() {
-    this.sub = this.dataService.getUsers().subscribe((usersRes)=> {
+    this.sub = this.dataService.getUsers().subscribe((usersRes) => {
       this.users = usersRes;
     })
   }
 
+
+  findUser(): Observable<boolean> {
+    return new Observable<boolean>((observer) => {
+      this.sub = this.dataService.getUsers().subscribe((usersRes) => {
+        this.users = usersRes;
+
+        const userWithEmail = this.users.find((user: any) => user.email === this.userEmail);
+
+        if (userWithEmail && userWithEmail.password === this.userPassword) {
+          observer.next(true);
+        } else {
+          observer.next(false);
+        }
+        observer.complete();
+      });
+    });
+  }
+
   async login() {
-
-    this.sub = this.dataService.getUsers().subscribe(async (usersRes)=> {
-      this.users = usersRes;
-      const emailExist = this.users.find((user:any)=> user.email === this.userEmail);
-      const passwordExist = this.users.find((user:any)=> user.password === this.userPassword);
-      console.log(emailExist);
-      console.log(passwordExist);
-      if(this.userEmail == null || this.userPassword == null) {
-        const warningAlert = this.alertCtrl.create({
-          header: "Warning",
-          subHeader: "Please fill in all fields",
-          buttons: ['OK']
-        });
-        (await warningAlert).present();
-      }else if(emailExist && passwordExist){
-        this.router.navigateByUrl('/home');
-      }else {
-        const warningAlert = this.alertCtrl.create({
-          header: "Warning",
-          subHeader: "Incorrect email or password",
-          buttons: ['OK']
-        });
-        (await warningAlert).present();
-      }
-    })
-
+    if (this.userEmail == null || this.userPassword == null) {
+      const warningAlert = this.alertCtrl.create({
+        header: "Warning",
+        subHeader: "Please fill in all fields",
+        buttons: ['OK']
+      });
+      (await warningAlert).present();
+    } else {
+      this.findUser().subscribe(async (userExists) => {
+        if (userExists) {
+          this.router.navigateByUrl('/home');
+        } else {
+          const warningAlert = this.alertCtrl.create({
+            header: "Warning",
+            subHeader: "Incorrect email or password",
+            buttons: ['OK']
+          });
+          (await warningAlert).present();
+        }
+      })
+    }
   }
 
   goToRegisterPage() {
@@ -73,30 +86,3 @@ export class LogInPage implements OnInit, OnDestroy{
 
 }
 
-    /*
-    console.log(emailExist);
-    console.log(passwordExist);
-
-
-    if(this.userEmail == null || this.userPassword == null) {
-      const warningAlert = this.alertCtrl.create({
-        header: "Warning",
-        subHeader: "Please fill in all fields",
-        buttons: ['OK']
-      });
-      (await warningAlert).present();
-    }else if(emailExist && passwordExist){
-      this.router.navigateByUrl('/home');
-    }else {
-      const warningAlert = this.alertCtrl.create({
-        header: "Warning",
-        subHeader: "Incorrect email or password",
-        buttons: ['OK']
-      });
-      (await warningAlert).present();
-    }
-
-  }
-    
-*/
-  
